ba4ca29426dbba48bf4a8ab27c18ffdc4b1819e3
Alex Sollman <alexndr.sollman@gmail.com>
2019-05-27 10:42:38 -0700

    Fixed issue where FE wasn't properly sending data to the API

--- public/js/script.js
+++ public/js/script.js
@@ -6,14 +6,19 @@ addComicForm.addEventListener('submit', e => {
     let formData = {}
 
     addComicForm.querySelectorAll('input').forEach(el => {
-        if (el.value.length > 1) {
+        if (el.value.length >= 1)
             formData[`${el.name}`] = el.value
-        }
+    })
+
+    addComicForm.querySelectorAll('select').forEach(el => {
+        var indexCheck = el.selectedIndex
+        if (el.selectedIndex)
+            formData[`${el.name}`] = el.options[el.selectedIndex].value
     })
 
     console.log(formData)
 
-    axios.post('/comics', JSON.stringify(formData))
+    axios.post('/comics', formData)
 
     /**
      * TODO: use axios to submit formData to /add-comic
