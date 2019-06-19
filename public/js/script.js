c0d58a35b105390baee5bee80a84301db6c0cb65
Alex Sollman <alexndr.sollman@gmail.com>
2019-06-18 23:14:47 -0700

    Class Based Script Refactored script.js to use classes

--- public/js/script.js
+++ public/js/script.js
@@ -1,51 +1,104 @@
-const addComicForm = document.querySelector('#add-comic')
-const comicList = document.querySelector('#comic-list')
-const deleteComics = document.querySelectorAll('.comic-delete')
+/* eslint-disable no-undef */
+/* eslint-disable no-underscore-dangle */
+;(() => {
+    class Comics {
+        constructor() {
+            this.addComicForm = document.querySelector('#add-comic')
+            this.comicList = document.querySelector('#comic-list')
+            this.deleteComics = document.querySelectorAll('.comic-delete')
+            this.formData = {}
+            this.comics = {}
 
-axios.get('/comics').then(data => {
-    const comics = data.data
+            this.bindEvents()
+        }
 
-    comics.forEach((comic) => {
-        let html = `<tr class="comic-info" id="${comic._id}">
-            <td class="comic-issue">${comic.issue ? comic.issue : ''}</td>    
-            <td class="comic-title">${comic.title ? comic.title : ''}</td>
-            <td class="comic-publisher">${comic.publisher ? comic.publisher : ''}</td>
-            <td class="comic-category">${comic.category ? comic.category : ''}</td>
-            <td class="comic-read">${comic.read ? 'yes' : 'no'}</td>
-            <td class="comic-delete"><a href="" data-id="${comic._id}" class="delete-comic">Delete</a></td>
-            </tr>`
+        bindEvents() {
+            if (this.addComicForm) {
+                this.addComicForm.addEventListener('submit', e => {
+                    e.preventDefault()
 
-        comicList.innerHTML += html
-    })
-})
+                    this.formData = {}
 
-addComicForm.addEventListener('submit', e => {
-    e.preventDefault()
+                    this.addComicForm.querySelectorAll('input').forEach(el => {
+                        if (el.value.length >= 1) {
+                            if (el.id === 'read') {
+                                this.formData[`${el.name}`] = el.checked
+                            } else {
+                                this.formData[`${el.name}`] = el.value
+                            }
+                        }
+                    })
 
-    let formData = {}
+                    this.addComicForm.querySelectorAll('select').forEach(el => {
+                        if (el.selectedIndex)
+                            this.formData[`${el.name}`] = el.options[el.selectedIndex].value
+                    })
 
-    addComicForm.querySelectorAll('input').forEach(el => {
-        if (el.value.length >= 1) {
-            if (el.id === 'read') {
-                formData[`${el.name}`] = el.checked
-            } else {
-                formData[`${el.name}`] = el.value
+                    axios
+                        .post('/comics', this.formData)
+                        .then(res => {
+                            console.log(res)
+                            this.getComics()
+                        })
+                        .catch(err => {
+                            // TODO: Replace conlog with modal or small alert.
+                            console.log(err)
+                        })
+                })
+            }
+
+            if (this.comicList) {
+                this.getComics()
+
+                this.comicList.addEventListener(
+                    'click',
+                    e => {
+                        e.preventDefault()
+                        if (e.target.className === 'delete-comic')
+                            this.deleteComic(e.target.dataset.id)
+                    },
+                    false,
+                )
             }
         }
-            
-    })
 
-    addComicForm.querySelectorAll('select').forEach(el => {
-        if (el.selectedIndex)
-            formData[`${el.name}`] = el.options[el.selectedIndex].value
-    })
+        getComics() {
+            this.comics = {}
+            axios
+                .get('/comics')
+                .then(res => {
+                    this.comics = res.data
 
-    axios.post('/comics', formData)
-})
+                    // TODO: Possibly move this to a different function, incase just fetching the data alone is needed.
+                    this.comicList.innerHTML = ''
 
-comicList.addEventListener('click', (e) => {
-    e.preventDefault()
-    if (e.target.className === 'delete-comic') {
-        axios.delete(`/comics/${e.target.dataset.id}`)
+                    this.comics.forEach(comic => {
+                        const html = `<tr class="comic-info" id="${comic._id}">
+                        <td class="comic-issue">${comic.issue ? comic.issue : ''}</td>    
+                        <td class="comic-title">${comic.title ? comic.title : ''}</td>
+                        <td class="comic-publisher">${comic.publisher ? comic.publisher : ''}</td>
+                        <td class="comic-category">${comic.category ? comic.category : ''}</td>
+                        <td class="comic-read">${comic.read ? 'yes' : 'no'}</td>
+                        <td class="comic-delete"><a href="" data-id="${
+                            comic._id
+                        }" class="delete-comic">Delete</a></td>
+                        </tr>`
+
+                        this.comicList.innerHTML += html
+                    })
+                })
+                .catch(err => {
+                    // TODO: Replace conlog with modal or small alert.
+                    console.log('Error', err)
+                })
+        }
+
+        deleteComic(id) {
+            axios.delete(`/comics/${id}`)
+            this.getComics()
+        }
     }
-})
+
+    // eslint-disable-next-line no-unused-vars
+    const comics = new Comics()
+})()
